ef5d08f7a1bdb363714a47440cee9c018649a965
Sunshine Daydream <alexbruner@live.com>
2018-06-06 13:03:50 -0500

    added website to master

---
+++ java.js
@@ -0,0 +1,73 @@
+//Nav Bar Buttons
+//Home Button
+$("#homeBtn").on("click", function(){
+    //Change Page Display
+    $("#homePage").removeClass("d-none");
+    $("#aboutPage").addClass("d-none");
+    $("#contactPage").addClass("d-none");
+    $("#portPage").addClass("d-none");
+    //Change Button Display
+    $("#homeBtn").addClass("border-highlight-nav border-secondary");
+    $("#aboutBtn").removeClass("border-highlight-nav border-secondary");
+    $("#portBtn").removeClass("border-highlight-nav border-secondary");
+    $("#contactBtn").removeClass("border-highlight-nav border-secondary");
+
+});
+//About Button
+$("#aboutBtn").on("click", function(){
+    $("#homePage").addClass("d-none");
+    $("#aboutPage").removeClass("d-none");
+    $("#contactPage").addClass("d-none");
+    $("#portPage").addClass("d-none");
+    //Change Button Display
+    $("#homeBtn").removeClass("border-highlight-nav border-secondary");
+    $("#aboutBtn").addClass("border-highlight-nav border-secondary");
+    $("#portBtn").removeClass("border-highlight-nav border-secondary");
+    $("#contactBtn").removeClass("border-highlight-nav border-secondary");
+
+});
+//Portfolio Button
+$("#portBtn").on("click", function(){
+    $("#homePage").addClass("d-none");
+    $("#aboutPage").addClass("d-none");
+    $("#contactPage").addClass("d-none");
+    $("#portPage").removeClass("d-none");
+    //Change Button Display
+    $("#homeBtn").removeClass("border-highlight-nav border-secondary");
+    $("#aboutBtn").removeClass("border-highlight-nav border-secondary");
+    $("#portBtn").addClass("border-highlight-nav border-secondary");
+    $("#contactBtn").removeClass("border-highlight-nav border-secondary");
+
+});
+function displayContact(){
+    $("#homePage").addClass("d-none");
+    $("#aboutPage").addClass("d-none");
+    $("#contactPage").removeClass("d-none");
+    $("#portPage").addClass("d-none");
+    //Change Button Display
+    $("#homeBtn").removeClass("border-highlight-nav border-secondary");
+    $("#aboutBtn").removeClass("border-highlight-nav border-secondary");
+    $("#portBtn").removeClass("border-highlight-nav border-secondary");
+    $("#contactBtn").addClass("border-highlight-nav border-secondary");
+};
+//Contact Button
+$("#contactBtn").on("click", function(){
+    displayContact()
+;});
+
+$("#openContactBtn").on("click", function(){
+    $("#contactMeForm").removeClass("d-none");
+    $("#openContactBtn").addClass("d-none");
+});
+$("#sendBtn").on("click", function(){
+    $("#contactMeForm").addClass("d-none");
+    $("#openContactBtn").removeClass("d-none");
+});
+
+$("#freelanceBtn").on("click", function(){
+    displayContact();
+});
+
+$("#corpBtn").on("click", function(){
+    displayContact();
+});
